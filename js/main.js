5f91ac55e210ca67bcde92dcc64804d4bbfa5223
Richard Houck <rdhouck@gmail.com>
2020-01-25 11:23:07 -0800

    updated stuff

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log(data.metalalbums[0]);
+var i;
+for (i = 0; i < 10; i++) {
+  var duration = data.metalalbums[i].Minutes;
+  var album = data.metalalbums[i].Album;
+  
+  var divElement = document.getElementsByClassName("album"+ (i+1))[0];
+  console.log(duration, album);
+  var albumbutton = document.getElementById("album" + (i + 1));
+  albumbutton.innerHTML = album;
+  // 60 minutes = 100%
+  // duration / 60 + 100 --> percentage
+  var percentage = (duration / 60) * 100; 
+//   var divHeight = duration * 10;
+  divElement.style.width = percentage + "%";
+} 
